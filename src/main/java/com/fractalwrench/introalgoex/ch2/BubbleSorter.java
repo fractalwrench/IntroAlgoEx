dca7fb4f2f0b8f86c76824bbdd663625a2c9c19d
Jamie <fractalwrench@gmail.com>
2016-11-23 08:27:45 +0000

    fail bubble sort test

---
+++ src/main/java/com/fractalwrench/introalgoex/ch2/BubbleSorter.java
@@ -0,0 +1,10 @@
+package com.fractalwrench.introalgoex.ch2;
+
+public class BubbleSorter implements IntSorter {
+
+    @Override
+    public void sort(int[] values) {
+
+    }
+
+}
---
+++ src/test/java/com/fractalwrench/introalgoex/ch2/BubbleSorterTest.java
@@ -0,0 +1,9 @@
+package com.fractalwrench.introalgoex.ch2;
+
+public class BubbleSorterTest extends IntSorterTest {
+
+    IntSorter providesIntSorter() {
+        return new BubbleSorter();
+    }
+
+}
